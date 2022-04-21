17e4ae2efeddb6c309f6f9920bc197db21eabf34
Rodrigo Borges <digodesign94@gmail.com>
2022-04-21 15:23:19 -0300

    faet: uping-ready

---
+++ src/components/ListItem/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+type ContainerProps = {
+    done: boolean;
+}
+
+export const Container = styled.div(( {done}: ContainerProps )=>(
+    `
+    display: flex;
+    background-color: #7b68ee;
+    padding: 10px;
+    border-radius: 10px;
+    margin-bottom: 10px;
+    align-items: center;
+
+    input {
+        width: 20px;
+        height: 20px;
+        margin-right: 5px;
+
+    }
+
+    label {
+         color: #000;
+         text-decoration: ${done ? 'line-through' : 'initial'};
+    }
+`
+));
